64233777ea07b7aad3c08697bca4eb993eae5cf4
Sonia Mezali <sonia.mezali@hotmail.fr>
2023-04-26 16:24:29 +0200

    js done

---
+++ index.js
@@ -0,0 +1,160 @@
+const startButton = document.getElementById("start-game");
+const newScore = document.querySelector("#score");
+let score = 0;
+const win = document.getElementById("win");
+let animationId;
+let gameInProgress = false;
+
+// start
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  if (gameInProgress) {
+    return;
+  }
+  gameInProgress = true;
+
+  window.addEventListener("keydown", (event) => {
+    const key = event.key;
+    if (!isMoving) {
+      isMoving = true;
+      switch (key) {
+        case "ArrowLeft":
+          event.preventDefault();
+          if (positionX > 0) positionX -= 40;
+          break;
+        case "ArrowRight":
+          event.preventDefault();
+          if (positionX < section1.offsetWidth - element1.offsetWidth)
+            positionX += 40;
+          break;
+        case "ArrowUp":
+          event.preventDefault();
+          if (positionY > 0) positionY -= 40;
+          break;
+        case "ArrowDown":
+          event.preventDefault();
+          if (positionY < section1.offsetHeight - element1.offsetHeight)
+            positionY += 40;
+          if (positionY > section1.offsetHeight - element1.offsetHeight) {
+            positionY = section1.offsetHeight - element1.offsetHeight;
+          }
+          break;
+      }
+
+      if (positionY < 0) {
+        positionY = 0;
+      }
+      if (positionX < 0) {
+        positionX = 0;
+      }
+
+      element1.style.setProperty("top", positionY + "px");
+      element1.style.setProperty("left", positionX + "px");
+      isMoving = false;
+    }
+  });
+
+  function fall() {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    if (ballX + ballWidth >= sectionWidth || ballX <= 0) {
+      ballSpeedX = -ballSpeedX;
+    }
+    if (ballY + ballHeight >= sectionHeight || ballY <= 0) {
+      ballSpeedY = -ballSpeedY;
+    }
+
+    const element1Rect = element1.getBoundingClientRect();
+    const ballRect = ball.getBoundingClientRect();
+
+    if (
+      ballRect.left < element1Rect.right &&
+      ballRect.right > element1Rect.left &&
+      ballRect.bottom > element1Rect.top &&
+      ballRect.top < element1Rect.bottom
+    ) {
+      score = 0;
+      newScore.textContent = score;
+    }
+
+    ball.style.setProperty("left", ballX + "px");
+    ball.style.setProperty("top", ballY + "px");
+
+    if (isFalling) {
+      fallBallY += fallBallSpeed;
+      fallingBall.style.top = `${fallBallY}px`;
+      fallingBall.style.left = `${fallBallX}px`;
+
+      const element1Rect = element1.getBoundingClientRect();
+      const fallingBallRect = fallingBall.getBoundingClientRect();
+
+      if (
+        fallingBallRect.left < element1Rect.right &&
+        fallingBallRect.right > element1Rect.left &&
+        fallingBallRect.bottom > element1Rect.top &&
+        fallingBallRect.top < element1Rect.bottom
+      ) {
+        fallBallX =
+          Math.random() * (section1.offsetWidth - fallingBall.offsetWidth);
+        fallBallY = 0;
+        score += 1;
+        newScore.textContent = score;
+      }
+
+      let sectionBounding = section1.getBoundingClientRect();
+
+      if (fallingBallRect.bottom >= sectionBounding.bottom) {
+        fallBallX =
+          Math.random() * (section1.offsetWidth - fallingBall.offsetWidth);
+        fallBallY = 0;
+      }
+      if (score === 30) {
+        win.showModal();
+        return cancelAnimationFrame(animationId);
+      }
+      animationId = requestAnimationFrame(fall);
+    }
+  }
+
+  animationId = requestAnimationFrame(fall);
+}
+
+//element 1 moving//
+
+const element1 = document.querySelector(".element1");
+const section1 = document.querySelector(".section1");
+
+let positionX = section1.offsetWidth / 2 - element1.offsetWidth / 2;
+let positionY = section1.offsetHeight / 2 - element1.offsetHeight / 2;
+
+element1.style.setProperty("top", positionY + "px");
+element1.style.setProperty("left", positionX + "px");
+
+let isMoving = false;
+
+//ball bouncing//
+
+const ball = document.querySelector(".ball");
+
+const sectionWidth = section1.offsetWidth;
+const sectionHeight = section1.offsetHeight;
+const ballWidth = ball.offsetWidth;
+const ballHeight = ball.offsetHeight;
+
+let ballX = Math.floor(Math.random() * (sectionWidth - ballWidth));
+let ballY = Math.floor(Math.random() * (sectionHeight - ballHeight));
+
+let ballSpeedX = 7;
+let ballSpeedY = 7;
+
+//falling element to catch//
+
+const fallingBall = document.querySelector(".fallingBall");
+
+let fallBallX =
+  Math.random() * (section1.offsetWidth - fallingBall.offsetWidth);
+let fallBallY = 0;
+let fallBallSpeed = 5;
+let isFalling = true;
